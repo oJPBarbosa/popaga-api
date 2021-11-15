c5cb29b63b7e833acce57853ba8d245b1118702d
oJPBarbosa <contato.ferb@gmail.com>
2021-11-15 15:31:26 -0300

    add atempt to work the cors out

--- src/app.js
+++ src/app.js
@@ -11,4 +11,17 @@ app.use(express.json());
 app.use(cors());
 app.use(routes);
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Header',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'GET', 'POST', 'PUT', 'DELETE');
+    return res.status(200).send();
+  }
+});
+
 module.exports = app;
