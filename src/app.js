e2bb650da97769f79d1e46e5fce3e8cb16aefbaf
oJPBarbosa <contato.ferb@gmail.com>
2021-11-15 18:27:16 -0300

    add file size limit to express.urlencoded()

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const routes = require('./routes');
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 app.use(routes);
 
