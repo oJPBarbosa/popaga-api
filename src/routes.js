0935cdc5b4d63d6a893218dad999929d1ee6c4ab
oJPBarbosa <contato.ferb@gmail.com>
2021-11-24 22:10:17 -0300

    update routes.js

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ const express = require('express');
 const auth = require('./middlewares/auth');
 
 const BillController = require('./controllers/BillController');
+const EventController = require('./controllers/EventController');
 const UserController = require('./controllers/UserController');
 
 const routes = express.Router();
@@ -12,6 +13,12 @@ routes.get('/bills/:id', auth, BillController.show);
 routes.post('/bills', auth, BillController.store);
 routes.put('/bills/:id', auth, BillController.update);
 routes.delete('/bills/:id', auth, BillController.destroy);
+
+routes.get('/events', auth, EventController.index);
+routes.get('/events/:id', auth, EventController.show);
+routes.post('/events', auth, EventController.store);
+routes.put('/events/:id', auth, EventController.update);
+routes.delete('/events/:id', auth, EventController.destroy);
 
 routes.get('/users', auth, UserController.index);
 routes.get('/users/:id', auth, UserController.show);
